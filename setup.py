ea512ece0e861bbe8f9040b03485f03474807207
Jazeps Basko <jazeps.basko@gmail.com>
2018-10-24 15:47:37 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name="strictus-dictus",
-    version="0.0.4",
+    version="0.0.5",
     url="https://github.com/jbasko/strictus-dictus",
     license="MIT",
     author="Jazeps Basko",
